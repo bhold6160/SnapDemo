d58d4bd244324e4c3c1119e94e01cc0c13ea807f
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-09-15 22:51:52 -0700

    layout set for imageViews for tabs

---
+++ app/src/main/java/com/example/brandonholderman/snapdemo/view/SnapTabsView.java
@@ -0,0 +1,67 @@
+package com.example.brandonholderman.snapdemo.view;
+
+import android.content.Context;
+import android.support.annotation.AttrRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.view.View;
+
+import com.example.brandonholderman.snapdemo.R;
+
+/**
+ * Created by brandonholderman on 9/15/17.
+ */
+
+public class SnapTabsView extends FrameLayout implements ViewPager.OnPageChangeListener {
+    private ImageView mCenterImage;
+    private ImageView mStartImage;
+    private ImageView mEndImage;
+    private ImageView mBottomImage;
+
+    private View mIndicator;
+
+    public SnapTabsView(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public SnapTabsView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SnapTabsView(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        init();
+    }
+
+    public void init() {
+        LayoutInflater.from(getContext()).inflate(R.layout.view_snap_tabs, this, false);
+
+        mCenterImage = (ImageView) findViewById(R.id.vst_center_image);
+        mStartImage = (ImageView) findViewById(R.id.vst_start_image);
+        mEndImage = (ImageView) findViewById(R.id.vst_end_image);
+        mBottomImage = (ImageView) findViewById(R.id.vst_bottom_image);
+
+        mIndicator = findViewById(R.id.vst_indicator);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+}
